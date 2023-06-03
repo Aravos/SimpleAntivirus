e661befcf32043c9a9ca6480ef1f3e67b1d16125
Aravos <sura.mitraa@gmail.com>
2023-06-03 12:55:14 +0530

    2nd commit

--- main.cpp
+++ main.cpp
@@ -5,30 +5,55 @@ using namespace std;
 void Scan(const std::string& directoryPath) {
     WIN32_FIND_DATA findData;
     HANDLE hFind = INVALID_HANDLE_VALUE;
-    std::string searchPath = directoryPath + "\\*";
+    std::string searchPath = directoryPath + "\\*";         
+    
+    //forming a complete search path using wildcard ch '*' in order to match multiple files or directories
 
     hFind = FindFirstFile(searchPath.c_str(), &findData);
+
+    // Using the Windows API to search for file in the specified directory
+
     if (hFind == INVALID_HANDLE_VALUE) {
         std::cout << "Error opening directory: " << directoryPath << std::endl;
         return;
     }
 
-    std::cout << "Files in directory: " << directoryPath << std::endl;
-    do {
-        if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-            // Skip directories
-            continue;
-        }
-        std::cout << findData.cFileName << std::endl;
-    } while (FindNextFile(hFind, &findData) != 0);
+    //Initiate loop to idetate over all files
+    while (1){
 
+        // Due to the fact that Windows lists oth current and parent dir of the file within the DIR itself
+        // If left unchecked and allowed to read them will cause confusion and may lead to an Infinite loop
+        // They will be excluded
+        // these files start with a '.' for current DIR and '..' for the parent DIR
+
+        if (strcmp(findData.cFileName, ".") == 0 || strcmp(findData.cFileName, "..") == 0) {
+            if (!FindNextFile(hFind, &findData)) {
+                break;    
+            }
+            continue; //Will Skip the rest of the loop
+        }
+
+        if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            // DIR
+            std::cout << "[DIR] " << findData.cFileName << std::endl;
+            // Recursive call to scan subdirectory Scan(fullPath);
+        } else {
+            // FILE
+            std::cout << "[FILE] " << findData.cFileName << std::endl;
+        }
+
+        // Updating Data of findData if it's = to 0 terminate the loop
+        if (FindNextFile(hFind, &findData) == 0) {
+            break;    
+        }
+    }
     FindClose(hFind);
 }
 
 int main() {
     std::string directoryPath;
-    cin >> directoryPath;
-    // = "C:\\Users\\Aravo\\OneDrive\\Desktop\\Antivirus";
+    std::cin >> directoryPath;
+
     Scan(directoryPath);
 
     return 0;
