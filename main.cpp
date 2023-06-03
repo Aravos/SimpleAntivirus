443bff0d419a145dc9c3c85d0f8df8e5a7fa82df
Aravos <sura.mitraa@gmail.com>
2023-06-03 14:19:18 +0530

    3rd commit

--- main.cpp
+++ main.cpp
@@ -1,26 +1,42 @@
 #include <iostream>
 #include <windows.h>
+#include <vector>
 using namespace std;
 
-void Scan(const std::string& directoryPath) {
+struct Node {
+    std::string path;
+    bool isDirectory;
+    struct Node* Parent;
+    std::vector<Node*> children;
+    // So as to vary the number of children
+};
+
+
+void Scan(const std::string& directoryPath, Node *node) {
     WIN32_FIND_DATA findData;
-    HANDLE hFind = INVALID_HANDLE_VALUE;
-    std::string searchPath = directoryPath + "\\*";         
+    HANDLE hFind = INVALID_HANDLE_VALUE;           
+    std::string searchPath = directoryPath + "\\*"; 
+    
+    //std::cout << "PASS 1" << std::endl;
     
     //forming a complete search path using wildcard ch '*' in order to match multiple files or directories
 
     hFind = FindFirstFile(searchPath.c_str(), &findData);
-
+    // hFind was Initially set to INVALID_HANDLE_VALUE it will not be updated unless there an existing DIR
+    // Additionally FindNextFile will return 0 when the search is over i.e all files are exhausted
     // Using the Windows API to search for file in the specified directory
 
     if (hFind == INVALID_HANDLE_VALUE) {
         std::cout << "Error opening directory: " << directoryPath << std::endl;
+        //std::cout << "FAIL 1" << std::endl;
         return;
     }
 
     //Initiate loop to idetate over all files
     while (1){
 
+        //std::cout << "PASS 2" << std::endl;
+        
         // Due to the fact that Windows lists oth current and parent dir of the file within the DIR itself
         // If left unchecked and allowed to read them will cause confusion and may lead to an Infinite loop
         // They will be excluded
@@ -33,13 +49,21 @@ void Scan(const std::string& directoryPath) {
             continue; //Will Skip the rest of the loop
         }
 
+        Node* newNode = new Node;
+        newNode->Parent = node;
         if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
             // DIR
             std::cout << "[DIR] " << findData.cFileName << std::endl;
-            // Recursive call to scan subdirectory Scan(fullPath);
+            newNode->path = directoryPath + "\\" + findData.cFileName;
+            newNode->isDirectory = true;
+            node->children.push_back(newNode);
+            Scan(newNode->path,newNode);
         } else {
             // FILE
             std::cout << "[FILE] " << findData.cFileName << std::endl;
+            newNode->path = directoryPath + "\\" + findData.cFileName;
+            newNode->isDirectory = false;
+            node->children.push_back(newNode);
         }
 
         // Updating Data of findData if it's = to 0 terminate the loop
@@ -54,7 +78,11 @@ int main() {
     std::string directoryPath;
     std::cin >> directoryPath;
 
-    Scan(directoryPath);
-
+    // C:\\Users\\Aravo\\OneDrive\\Desktop\\Antivirus
+    // Creating pointer to the Parent node and assigning it space
+    Node* node = new Node;
+    node->isDirectory = true;
+    node->path = directoryPath;
+    Scan(directoryPath,node);
     return 0;
 }
